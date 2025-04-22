edb4c7e0fca71d52dfb4cb3110a1902bdcaac879
Kavya Singh <150460816+kavyasingh020805@users.noreply.github.com>
2025-04-22 22:28:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images/255, testing_images/255
+
+class_names=['Plane','Car','Bird','Cat','Deer','Dog','Frog','Horse','Ship','Truck']
