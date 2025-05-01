2a215250d3f304d1c32ec1d63f7e739220fb1522
Kavya Singh <150460816+kavyasingh020805@users.noreply.github.com>
2025-05-01 23:06:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ testing_labels = testing_labels[:4000]
 
 model = models.Sequential
 model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooloing2D((2,2)))
+model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3), activation='relu'))
-model.add(layers.MaxPooloing2D((2,2)))
+model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
